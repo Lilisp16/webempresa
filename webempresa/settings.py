4e1e992148146ec45c5e96a19160d813f500c24e
Lilisp16 <lijhosa@gmail.com>
2025-05-22 18:53:18 -0500

    Configurar PostgreSQL en Render

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -1,6 +1,5 @@
+import dj_database_url
 from decouple import config
-
-
 
 """
 Django settings for webempresa project.
@@ -88,10 +87,9 @@ WSGI_APPLICATION = 'webempresa.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+       dj_database_url.parse(config('DATABASE_URL'))
+    
 }
 
 
